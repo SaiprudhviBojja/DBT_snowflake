bfa287fc40c1902864a4310f94a42672e2fefa1b
sai prudhvi <s38066611@gmail.com>
2024-05-24 07:55:32 +0000

    commit changes

--- models/hr_sample_database/COUNTRIES.sql
+++ models/hr_sample_database/COUNTRIES.sql
@@ -1,4 +1,4 @@
-create or replace view HR_SAMPLE_DATABASE.dbt_sprudhvi.COUNTRIES
+create or replace view HR_SAMPLE_DATABASE.dbt_snowflake.COUNTRIES
 as (
     SELECT *
     FROM hr_sample_database.countries
